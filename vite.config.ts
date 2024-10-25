7d2d4f2fddd97890c66aa4b67c302f065a4171cd
yukinko05 <grey.sora.35@gmail.com>
2024-10-26 08:50:40 +0900

    github pagesで成果物を見れるようにする

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? "/dog-photo-viewer" : "/",
   plugins: [react()],
   resolve: {
     alias: {
