50336f1a9fcb55d51be14364899b4827cdbf271b
yukinko05 <grey.sora.35@gmail.com>
2024-10-26 01:13:54 +0900

    ホーム画面にヘッダーを適用

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
-
+import Header from "@/components/Header";
 const Home = () => {
   return (
     <div>
-      Home
+      <Header title={"Breed Buddies"} backgroundImage={"https://time-fun.fun/wp-content/uploads/2020/07/twitter-dog-14-1.jpg"} />
     </div>
   )
 }
