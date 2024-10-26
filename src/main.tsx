159d0a44661af04ef437e7caaed0110d826b4c90
yukinko05 <grey.sora.35@gmail.com>
2024-10-26 09:03:47 +0900

    github pageでホスティングしたときに正しく動くようにbasenameを切り替えるように修正

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Home from './pages/Home';
-import { Provider } from 'react-redux';
-import { store } from './app/store.ts';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import { Provider } from "react-redux";
+import { store } from "./app/store.ts";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ],
   {
-    path: "/",
-    element: <Home />,
-  },
-]);
+    basename: import.meta.env.DEV ? "/" : "/dog-photo-viewer/",
+  }
+);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
